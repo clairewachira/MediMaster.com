a9aeeb68ee1ca5180887309443975a95b71440c5
Claire Wanjiru <claire.wachira@strathmore.edu>
2023-06-18 18:45:49 +0300

    first commit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+$conn = mysqli_connect("localhost", "root", "","drug_dispensing");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connnected successfully";
+?>
